b40848ed14063d79fde6382837c3a23f3ca46138
samin <samintejas@gmail.com>
2024-06-13 18:42:42 +0530

    basic ui , v1.0.0

--- Frontend/src/app/contact/page.tsx
+++ Frontend/src/app/contact/page.tsx
@@ -1,12 +1,29 @@
 import styles from "./page.module.css";
 import Navbar from "@/components/navbar";
 
+interface ContactForm {
+  message: string;
+  email: string;
+}
+
 export default function Contact() {
   return (
     <div>
-      <Navbar/>
-      <h2>notes</h2>
-      <p>COntect</p>
+      <Navbar />
+      <div className={styles.contact}>
+        <h2></h2>
+        <form method="POST" action="/your/server/endpoint">
+          <div className={styles.input}>
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" name="message" required></textarea>
+          </div>
+          <div className={styles.input}>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <button type="submit">Get in touch !</button>
+        </form>
+      </div>
     </div>
   );
 }
